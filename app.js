cbd5360239eb25a1671f2de5385810828f23d9e9
nulink <56325114+nulink@users.noreply.github.com>
2020-06-11 16:50:11 +0100

    asd

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 80,
+        "value": 1020,
         "density": {
           "enable": true,
           "value_area": 800
