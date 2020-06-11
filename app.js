43d97d11ce5472fec42467869e7c937b74ecc945
nulink <56325114+nulink@users.noreply.github.com>
2020-06-11 16:56:40 +0100

    das

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 1020,
+        "value": 80,
         "density": {
           "enable": true,
           "value_area": 800
